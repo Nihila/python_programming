d508bc4412115d15662a4d9258e093b741355d84
Nihila <36109188+Nihila@users.noreply.github.com>
2018-02-04 14:55:55 +0530

    Add files via upload

---
+++ String2.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module5
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+sentence='programmer'
+print sentence.count('r')
+print sentence.find('g')
+print sentence.replace('m','y')
