e5a805c97659894a8913ff46cc3e476a0d22fb02
Nihila <36109188+Nihila@users.noreply.github.com>
2018-02-04 12:40:59 +0530

    Add files via upload

---
+++ positiveornegative.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+a=int(input("enter value"))
+if a<=0:
+    if a==0:
+        print("zero")
+    else:
+        print("negative")
+else:
+    print("positive")
